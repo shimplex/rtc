016f59de4e87d0e8ebc847af1c3c30ba8137acb8
shimplex <shimplex@gmail.com>
2019-08-14 22:50:50 +0900

    Initial

--- client/webrtc1.js
+++ client/webrtc1.js
@@ -16,11 +16,6 @@ let previous_received_bytes = 0;
   const meta = document.getElementById('js-meta');
   const sdkSrc = document.querySelector('script[src*=skyway]');
 
-  meta.innerText = `
-    UA: ${navigator.userAgent}
-    SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
-  `.trim();
-
   const localStream = await navigator.mediaDevices
     .getUserMedia({
       audio: false,
